3e30dfcda5280dc999e162ee6e8fc04f4dc80543
Vincent Fernandez <61900401+vincefernandez@users.noreply.github.com>
2020-12-06 18:47:13 +0800

    Update function.js

--- function.js
+++ function.js
@@ -1,13 +1,29 @@
-let count = 0;
-setInterval(function () {
-    count++;
-    if (count == 3) {
-        document.getElementById("a").style.color = "blue";
-        document.getElementById("a").style.width = "90%";
+var cta = document.querySelector(".cta");
+var i = document.querySelector("i");
+var check = 0;
+
+cta.addEventListener('click', function(e){
+    var text = e.target.nextElementSibling;
+    var loginText = e.target.parentElement;
+    text.classList.toggle('show-hide');
+    loginText.classList.toggle('expand');
+    if(check == 0)
+    {
+        cta.innerHTML = "<i class=\fa fa-chevron-down fa-2x\></i>";
+        i.innerHTML = "  <i class=\fa fa-chevron-down fa-2x\></i>";
+        cta.innerHTML = "Get Started!";
+        cta.innerHTML.title ="Get Started!";
+        check++;
     }
-    if (count == 6) {
-        document.getElementById("a").style.width = '0';
-        document.getElementById("a").style.color = "red";
-        count = 0;
+    else
+    {
+        i.innerHTML = "  <i class=\"fa fa-chevron-up fa-2x\></i>";
+        cta.innerHTML = "<i class=\"fa fa-chevron-up fa-2x\></i>";
+        cta.innerHTML = "LogIn Now!";
+        cta.innerHTML.title ="Log In Now!";
+        check = 0;
     }
-}, 500)
+})
+
+
+  
