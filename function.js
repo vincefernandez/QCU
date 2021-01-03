aad31195b0a9d0b356d858ee3ecfb10ea9c5141c
Vincent Fernandez <61900401+vincefernandez@users.noreply.github.com>
2021-01-03 17:39:50 +0800

    Update function.js

--- function.js
+++ function.js
@@ -23,5 +23,41 @@ cta.addEventListener('click', function(e){
     }
 })
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
-  
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+// window.addEventListener('scroll',() => {
+//     const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+//     const scroll = window.scrollY;
+//     if(Math.ceil(scroll) === scrollable){
+//         alert ('nice');
+//     }
+// })
+
+// function disableScroll() { document.body.style.overflow="hidden";  }
+// disableScroll();)
